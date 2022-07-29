71725f3df24c05ccd983cd40deb97146c119d1d7
elizabethchibvuri <elizabeth.chibvuri@chillisoft.co.za>
2022-07-29 16:19:21 +0200

    Added neighbor checking code and test

--- source/GameOfLife/game-of-life.ts
+++ source/GameOfLife/game-of-life.ts
@@ -1,4 +1,4 @@
-import { Cell } from "./Cell"
+import { Cell } from './Cell'
 
 /* 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
    2. Any live cell with more than three live neighbours dies, as if by overcrowding.
@@ -6,27 +6,58 @@ import { Cell } from "./Cell"
    4. Any dead cell with exactly three live neighbours becomes a live cell. 
 */
 
-export class GameOfLife{
-    board: Array<Array<number>>
-    constructor(board: Array<Array<number>>) {
-      this.board = board
+export class GameOfLife {
+  board: Array<Array<number>>
+  newBoard: Array<Array<number>>
+  constructor(board: Array<Array<number>>) {
+    this.board = board
+  }
+
+  execute(): Array<Array<number>> {
+    if (this.board.length === 0) {
+      return this.board
     }
 
-     execute(): Array<Array<number>>{
-        if(this.board.length === 0){
-            return this.board
+    //iterate row by column
+    //for each cell positon get # of live neighboughs
+    //check and set new state
+    let cellsToKill: Cell[] = []
+    for (var i = 0; i < this.board.length; i++) {
+      for (var j = 0; j < this.board[i].length; j++) {
+        let cellValue = this.board[i][j]
+        if (cellValue === 1) {
+          const neighboughs = 0
+          const north = this.board[i - 1][j]
+          const northEast = this.board[i - 1][j + 1]
+          const northWest = this.board[i - 1][j - 1]
+          const south = this.board[i + 1][j]
+          const southEast = this.board[i + 1][j + 1]
+          const southWest = this.board[i + 1][j - 1]
+          const east = this.board[i][j + 1]
+          const west = this.board[i][j - 1]
+
+          if (north) neighboughs + north
+          if (northEast) neighboughs + northEast
+          if (northWest) neighboughs + northWest
+          if (south) neighboughs + south
+          if (southEast) neighboughs + southEast
+          if (southWest) neighboughs + southWest
+          if (southEast) neighboughs + southEast
+          if (east) neighboughs + east
+          if (west) neighboughs + west
+
+          if (neighboughs < 2) {
+            const cell: Cell = { row: i, column: j }
+            cellsToKill.push(cell)
+          }
         }
-        let cellPosition :Cell = {row :0 ,column :0}
-        for(var i = 0;i< this.board.length ;i++){
-            for(var j =0; j < this.board[i].length ; j++){
-                 let cellValue = this.board[i][j]
-                if(cellValue === 1 ){
-                  cellPosition.row = i
-                  cellPosition.column =j
-                }
-            }
-        }
-        this.board[cellPosition.row][cellPosition.column] = 0
-        return this.board
+      }
     }
+
+    cellsToKill.forEach((cell) => {
+      this.board[cell.row][cell.column] = 0
+    })
+
+    return this.board
+  }
 }
--- source/GameOfLifeTests/game-of-life-tests.spec.ts
+++ source/GameOfLifeTests/game-of-life-tests.spec.ts
@@ -1,57 +1,128 @@
 import { describe, expect, it } from '@jest/globals'
 import { GameOfLife } from '../GameOfLife/game-of-life'
 
-describe('my first test',()=>{
-    it('should expect one',async ()=>{
-        expect(1).toEqual(1)
-    })
+describe('my first test', () => {
+  it('should expect one', async () => {
+    expect(1).toEqual(1)
+  })
 })
 
-describe('constructor',()=>{
-    it('should expect empty board if empty board is parsed through',async ()=>{
-        // Arrange 
-        let board = []
-        //Act
-        let gameOfLife = new GameOfLife(board)
-        //Assert
-        expect(gameOfLife.board).toEqual(board)
-    })
-    it('should expect populated board if board is parsed through',async ()=>{
-        // Arrange 
-        let board = [[0,0,0],[0,1,0],[0,0,0]]
-        //Act
-        let gameOfLife = new GameOfLife(board)
-        //Assert
-        expect(gameOfLife.board).toEqual(board)
-    })
+describe('constructor', () => {
+  it('should expect empty board if empty board is parsed through', async () => {
+    // Arrange
+    let board = []
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    //Assert
+    expect(gameOfLife.board).toEqual(board)
+  })
+  it('should expect populated board if board is parsed through', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    //Assert
+    expect(gameOfLife.board).toEqual(board)
+  })
 })
-describe('execute',()=>{
-    it('should return an empty board if intial board is empty',async ()=>{
-        // Arrange 
-        let board = []
-        //Act
-        let gameOfLife = new GameOfLife(board)
-        let result = gameOfLife.execute()
-        //Assert
-        expect(result).toEqual(board)
-    })
-    it('should return all dead cells if inital board only has dead cells',async ()=>{
-        // Arrange 
-        let board = [[0,0,0],[0,0,0],[0,0,0]]
-        //Act
-        let gameOfLife = new GameOfLife(board)
-        let result = gameOfLife.execute()
-        //Assert
-        expect(result).toEqual(board)
-    })
-    it('should die if cell has fewer than two neighbours',async ()=>{
-        // Arrange 
-        let board = [[0,0,0],[0,1,0],[0,0,0]]
-        //Act
-        let gameOfLife = new GameOfLife(board)
-        let result = gameOfLife.execute()
-        //Assert
-        let expected = [[0,0,0],[0,0,0],[0,0,0]]
-        expect(result).toEqual(expected)
-    })
+describe('execute', () => {
+  it('should return an empty board if intial board is empty', async () => {
+    // Arrange
+    let board = []
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    expect(result).toEqual(board)
+  })
+  it('should return all dead cells if inital board only has dead cells', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    expect(result).toEqual(board)
+  })
+  it('should die if cell has fewer than two neighbours', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    let expected = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+    expect(result).toEqual(expected)
+  })
+  it('should die if cell has 1 neighbour', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [1, 1, 0],
+      [0, 0, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    let expected = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+    expect(result).toEqual(expected)
+  })
+
+  it('should die if cell has 1 neighbour to the east', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    let expected = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+    expect(result).toEqual(expected)
+  })
+  it('should die if cell has 1 neighbour to the south', async () => {
+    // Arrange
+    let board = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 1, 0],
+    ]
+    //Act
+    let gameOfLife = new GameOfLife(board)
+    let result = gameOfLife.execute()
+    //Assert
+    let expected = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]
+    expect(result).toEqual(expected)
+  })
 })
